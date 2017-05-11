0415aea93ffe75823cc9897eba49d5cbe4351eb5
magda michalska <magdalena.alicja.michalska@gmail.com>
2017-05-11 20:51:58 +0000

    cleanups

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 let http = require('http');
 let express = require('express');
+//TODO: add multiplayer...
 //let socketio = require('socket.io');
-
 //var app = connect().use(connect.static('public')).listen(3000);
 
 let app = express();
@@ -11,7 +11,7 @@ let server = http.createServer(app);
 //let io = socketio(server);
 
 app.use(express.static(__dirname + '/dist'));
-//server.listen(process.env.PORT, () => console.log('Ready to work!'));
-server.listen(8080, () => console.log('Ready to work!'));
+server.listen(process.env.PORT, () => console.log('Ready to work!'));
+//server.listen(8080, () => console.log('Ready to work!'));
 
 
